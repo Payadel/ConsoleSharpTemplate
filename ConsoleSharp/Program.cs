83feab378ace484db709a7ee9c30aa01dca67dc5
Hamid Molareza <molareza@protonmail.com>
2024-10-29 16:11:37 +0330

    fix(logging): use appsettings.json configurations

--- ConsoleSharp/Program.cs
+++ ConsoleSharp/Program.cs
@@ -8,14 +8,17 @@ using Microsoft.Extensions.Configuration;
 
 var services = new ServiceCollection();
 
-// Add logging
-services.AddLogging(builder => builder.AddConsole());
-
 // Add configuration
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
 services.AddSingleton<IConfiguration>(configuration);
+
+// Add logging using configuration
+services.AddLogging(builder => {
+    builder.AddConfiguration(configuration.GetSection("Logging")); // Bind to "Logging" section
+    builder.AddConsole();
+});
 
 var appSettings = configuration.GetSection("App")
     .Get<AppSettings>() ?? throw new ArgumentException("Can not load app settings data.");
