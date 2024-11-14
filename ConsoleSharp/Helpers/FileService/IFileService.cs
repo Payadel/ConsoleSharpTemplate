3b4bb718439742f04de61fb8bcfb512864899099
HamidMolareza <molareza@protonmail.com>
2024-11-14 12:51:42 +0330

    fix!: use `System.IO.Abstractions` to help mocking + Improve tests

--- ConsoleSharp/Helpers/FileService/IFileService.cs
+++
@@ -1,6 +0,0 @@
-namespace ConsoleSharpTemplate.Helpers.FileService;
-
-// This help to mock File class in tests
-public interface IFileService {
-    public bool Exists(string path);
-}
--- TestConsoleSharp/CommandLineTests.cs
+++ TestConsoleSharp/CommandLineTests.cs
@@ -1,19 +1,24 @@
+using System.IO.Abstractions.TestingHelpers;
 using ConsoleSharpTemplate;
 using ConsoleSharpTemplate.Settings;
-using TestConsoleSharp.Mocks;
 
 namespace TestConsoleSharp;
 
 public class CommandLineTests {
     private readonly AppSettings _settings = new();
-    private readonly MockFileService _fileSystem = new();
+    private readonly MockFileSystem _fileSystem = new();
+    private const string ExistFileName = "file.txt";
 
-    [Fact]
-    public async Task InvokeAsync_WithValidInputs_ReturnsSuccessCode() {
+    public CommandLineTests() {
+        _fileSystem.AddFile(ExistFileName, string.Empty);
+    }
+
+    [Theory]
+    [InlineData("-f", "-d", 10)]
+    [InlineData("--file", "--delay", 10)]
+    public async Task InvokeAsync_WithValidInputs_ReturnsSuccessCode(string fileFlag, string delayFlag, int delay) {
         // Arrange
-        const int delay = 10;
-        const string filePath = "exist-file.txt";
-        var args = new[] { "-f", filePath, "-d", delay.ToString() };
+        var args = new[] { fileFlag, ExistFileName, delayFlag, delay.ToString() };
 
         // Act
         var result = await CommandLine.InvokeAsync(args, _settings, _fileSystem);
@@ -21,15 +26,14 @@ public class CommandLineTests {
         // Assert
         Assert.Equal(0, result); // Assuming success returns 0
         Assert.Equal(delay, _settings.Delay);
-        Assert.Equal(filePath, _settings.SampleFile);
+        Assert.Equal(ExistFileName, _settings.SampleFile);
     }
 
     [Fact]
     public async Task InvokeAsync_MissingFile_ReturnsError() {
         // Arrange
         const int delay = 10;
-        const string filePath = "";
-        var args = new[] { "-f", filePath, "-d", delay.ToString() };
+        var args = new[] { "-f", string.Empty, "-d", delay.ToString() };
 
         // Act
         var result = await CommandLine.InvokeAsync(args, _settings, _fileSystem);
